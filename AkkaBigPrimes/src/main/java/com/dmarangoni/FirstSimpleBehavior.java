385e17c4c9f895f85d30edc8e8f7929712f36f2f
darlan <marangoni.darlan1@gmail.com>
2024-11-14 13:02:27 -0300

    create first simple behavior

---
+++ AkkaBigPrimes/src/main/java/com/dmarangoni/FirstSimpleBehavior.java
@@ -0,0 +1,23 @@
+package com.dmarangoni;
+
+import akka.actor.typed.Behavior;
+import akka.actor.typed.javadsl.AbstractBehavior;
+import akka.actor.typed.javadsl.ActorContext;
+import akka.actor.typed.javadsl.Behaviors;
+import akka.actor.typed.javadsl.Receive;
+
+public class FirstSimpleBehavior extends AbstractBehavior<String> {
+
+    private FirstSimpleBehavior(ActorContext<String> context) {
+        super(context);
+    }
+
+    public static Behavior<String> create() {
+        return Behaviors.setup(FirstSimpleBehavior::new);
+    }
+
+    @Override
+    public Receive<String> createReceive() {
+        return null;
+    }
+}
